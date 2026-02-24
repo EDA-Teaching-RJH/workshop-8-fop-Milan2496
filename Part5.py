6ad643a13a70c4f602d45c37b4fdea612b73aceb
Milan2496 <mg101@kent.ac.uk>
2026-02-24 13:04:50 +0000

    Challenge 2

--- Part5.py
+++ Part5.py
@@ -2,7 +2,7 @@ import re
 
 phone = input("What is your phone number?")
 
-if re.search(r"^07\d{9}$", phone): 
+if re.search(r"^[A-Za-z]{4}[0-9]{4}$", phone): 
     print("Valid")
 else: 
     print("Invalid")
